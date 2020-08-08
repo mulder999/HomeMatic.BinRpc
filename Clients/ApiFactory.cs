7b66ee2d81669627bd7e6d03cca8889eada789b7
mulder999 <nospam099-github@yahoo.com>
2020-08-08 23:59:50 +0200

    Better factory

---
+++ Clients/ApiFactory.cs
@@ -0,0 +1,23 @@
+﻿using CreativeCoders.DynamicCode.Proxying;
+using CreativeCoders.Net.Http;
+using CreativeCoders.Net.XmlRpc.Client;
+using CreativeCoders.Net.XmlRpc.Proxy;
+using CreativeCoders.Net.XmlRpc.Proxy.Analyzing;
+using System.Net.Http;
+
+namespace HomeMaticBinRpc.Clients
+{
+    public static class ApiFactory
+    {
+
+        public static T GetApi<T>(bool useXml, string url)
+            where T : class
+        {
+            var client = useXml
+                ? (IXmlRpcClient)new XmlRpcClient(new HttpClientEx(new HttpClient())) { Url = url }
+                : new BinRpcClient(url);
+            return new ProxyBuilder<T>()
+                .Build(new XmlRpcProxyInterceptor<T>(client, new ApiAnalyzer<T>().Analyze()));
+        }
+    }
+}
